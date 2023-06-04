a3e40cbc9bdaab5b157c15b1e2b1c8df436aef22
okm321 <naofumi.okamoto@casting-one.jp>
2023-06-04 16:44:06 +0900

    headerButtonコンポーネント作成

--- src/components/button/HeaderButton/HeaderButton.stories.tsx
+++ src/components/button/HeaderButton/HeaderButton.stories.tsx
@@ -1,14 +1,33 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { HeaderButtonInner } from './HeaderButton'
+import { HeaderButton } from './HeaderButton'
 
-type Story = StoryObj<typeof HeaderButtonInner>
+type Story = StoryObj<typeof HeaderButton>
 
-const meta: Meta<typeof HeaderButtonInner> = {
+const meta: Meta<typeof HeaderButton> = {
   title: 'Button/HeaderButton',
-  component: HeaderButtonInner,
+  component: HeaderButton,
+  args: {
+    label: 'カート',
+    icon: '/images/common/cart.png',
+  },
 }
 
 export default meta
 
 export const Base: Story = {}
+
+export const Linkable: Story = {}
+Linkable.args = {
+  link: 'https://yahoo.co.jp',
+}
+
+export const ReverseColor: Story = {}
+ReverseColor.args = {
+  colorReverse: true,
+}
+
+export const Count: Story = {}
+Count.args = {
+  count: 5,
+}
--- src/components/button/HeaderButton/HeaderButton.tsx
+++ src/components/button/HeaderButton/HeaderButton.tsx
@@ -1,23 +1,64 @@
 import { FC, ReactNode } from 'react'
 import Style from './HeaderButton.module.scss'
+import Link from 'next/link'
 
-// type HeaderButtonProps = {
-//   /** ラベル */
-//   label: string
-//   /** アイコン */
-//   icon?: ReactNode
-//   /** リンク先 */
-//   link?: string
-//   /** 色反転 */
-//   colorReverse?: boolean
-// }
-
-export const HeaderButtonInner: FC = () => {
-  return <div className={Style.buttonItem__text}>トップ</div>
+type HeaderButtonInnerProps = {
+  /** ラベル */
+  label: string
+  /** アイコン */
+  icon?: string
+  /** アイコンに数を付与 */
+  count?: number
 }
 
-// export const HeaderButtonWrapper: FC<HeaderButtonProps> = ({
-//   link
-// }) => {
-//   if (link != null) return <div>{HeaderButtonInner()}</div>
-// }
+type HeaderButtonProps = HeaderButtonInnerProps & {
+  /** リンク先 */
+  link?: string
+  /** 色反転 */
+  colorReverse?: boolean
+}
+
+export const HeaderButtonInner: FC<HeaderButtonInnerProps> = ({
+  label,
+  icon,
+  count,
+}) => {
+  return (
+    <>
+      <div className={Style.buttonItem__img}>
+        <img src={icon} className={Style.icon} />
+        {count && <div className={Style.count}>{count}</div>}
+      </div>
+      <div className={Style.buttonItem__text}>{label}</div>
+    </>
+  )
+}
+
+export const HeaderButton: FC<HeaderButtonProps> = ({
+  label,
+  icon,
+  link,
+  colorReverse,
+  count,
+}) => {
+  if (link == null)
+    return (
+      <button
+        className={`${Style.buttonItem} ${
+          colorReverse ? Style.buttonItemReverse : ''
+        }`}
+      >
+        <HeaderButtonInner label={label} icon={icon} count={count} />
+      </button>
+    )
+  return (
+    <Link
+      href={link}
+      className={`${Style.buttonItem} ${
+        colorReverse ? Style.buttonItemReverse : ''
+      }`}
+    >
+      <HeaderButtonInner label={label} icon={icon} count={count} />
+    </Link>
+  )
+}
